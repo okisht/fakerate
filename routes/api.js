c0f305d77bb66b7db4a2530663dc00cb0b3f61c2
okisht <oguz.avci@PROJE.LOCAL>
2019-01-25 17:34:39 +0300

    updates

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const FirstModel = require('../models/productmodel');
-const viewPage = "<p>test</p>"
 
 // get list from db
 router.get('/v1/:productid', function(req, res){
         FirstModel.findOne({productid:req.params.productid}).then(function(model){
-            res.send (model +  viewPage);
+            res.send (
+                 model
+                );
         });
 });
 
 
 // add new data to db
 router.post('/v1', function(req, res){
+    /* 
+    const cursor = db.getCollection('models').find( {"productid" : "pMxPqlB"});
+    if (cursor.length > 0)
+    { 
+            res.send(console.log("this img already uploaded"));
+    }
+    else {
+        FirstModel.create(req.body).then(function(model){
+            res.send (model);
+        });
+    }
+
+    */
     FirstModel.create(req.body).then(function(model){
         res.send (model);
     });
